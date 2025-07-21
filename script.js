e139638a377f7e68c34f0d950d91fca9ebf16437
Omi Co., Ltd. <95911788+Omi-Co-Ltd@users.noreply.github.com>
2025-07-21 11:08:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const links = [
-  { name: "Google", url: "https://www.google.com" },
-  { name: "YouTube", url: "https://www.youtube.com" },
-  { name: "Wikipedia", url: "https://www.wikipedia.org" },
-  { name: "GitHub", url: "https://github.com" }
+  { name: "尾身株式会社 公式ホームページ", url: "https://omi-co-ltd.github.io/on/" },
+  { name: "オンラインストア", url: "https://docs.google.com/forms/d/e/1FAIpQLSerigx5sZM1B-VG9dNvKMQ72soiATaavHCtYwkQoQtjblK-jw/viewform?usp=sf_link" },
+  { name: "OMIPASS", url: "https://omi-co-ltd.github.io/omipass/" },
+  { name: "OmiMovie", url: "https://sites.google.com/view/new-omi-movie-jp/%E3%83%9B%E3%83%BC%E3%83%A0" },
+  { name: "OMI CINEMA", url: "https://omicinema-reservation.reserven.jp/" },
+  { name: "OMI HOTEL", url: "https://omi-co-ltd.github.io/omihotel/" },
+  { name: "NEWS OMI", url: "https://omi-co-ltd.github.io/news/" },
+  { name: "尾身天堂", url: "https://omi-co-ltd.github.io/omitendo/" },
+  { name: "おみ日記", url: "https://omi-co-ltd.github.io/ominikki/" },
+  { name: "お問い合わせフォーム", url: "https://docs.google.com/forms/d/e/1FAIpQLSfbVolFZMITt5DCv72DeYjkBU4YGPfXohDqqs2gKyhaq2SYRQ/viewform?usp=sf_link" }
 ];
 
 const listElement = document.getElementById("link-list");
